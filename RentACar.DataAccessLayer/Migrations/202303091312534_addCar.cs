01e43b2e4b800879dc3b917eca0b236e8c9d0760
yusuferturkk <yusuf_erturk98@hotmail.com>
2023-03-09 16:20:30 +0300

    Add project files.

---
+++ RentACar.DataAccessLayer/Migrations/202303091312534_addCar.cs
@@ -0,0 +1,35 @@
+﻿namespace RentACar.DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addCar : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cars",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Brand = c.String(),
+                        Model = c.String(),
+                        ModelYear = c.Int(nullable: false),
+                        Km = c.Int(nullable: false),
+                        Color = c.String(),
+                        Price = c.Double(nullable: false),
+                        Image1 = c.String(),
+                        Image2 = c.String(),
+                        Image3 = c.String(),
+                        Image4 = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Cars");
+        }
+    }
+}
